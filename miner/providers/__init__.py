63c08aea0d0bee19828e234ee7ffe73abfa2561e
aaron goecke <panta@aarons-MBP.fritz.box>
2024-09-02 22:45:48 +0200

    good

--- miner/providers/__init__.py
+++ miner/providers/__init__.py
@@ -1,10 +1,10 @@
 from .anthropic import Anthropic
 
-# from .anthropicbedrock import AnthropicBedrock
+from .anthropicbedrock import AnthropicBedrock
 from .bedrock import Bedrock
 from .gemini import Gemini
 from .groq import Groq
 from .open_ai import OpenAI
 
 # __all__ = [Anthropic, AnthropicBedrock, Bedrock, Gemini, Groq, OpenAI]
-__all__ = [Anthropic, Bedrock, Gemini, Groq, OpenAI]
+__all__ = [Anthropic, AnthropicBedrock, Bedrock, Gemini, Groq, OpenAI]
